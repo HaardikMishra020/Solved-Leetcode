a8c04c0e0d01ba55b12abca35c198064db47f765
Haardik Mishra <117261665+HaardikMishra020@users.noreply.github.com>
2025-08-30 23:10:53 +0530

    Time: 0 ms (100%), Space: 42.3 MB (42.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,25 +1,24 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        Stack<Integer>st=new Stack<>();
-        int i=0;
-        int j=0;
-        while(i<m&&j<n){
-            if(nums1[i]<nums2[j]){
-                st.push(nums1[i]);
-                i++;
+        int i=m-1;
+        int j=n-1;
+        int x=m+n-1;
+        while(i>=0&&j>=0){
+            if(i>=0&&nums1[i]>nums2[j]){
+                nums1[x]=nums1[i];
+                i--;
             }
             else{
-                st.push(nums2[j]);
-                j++;
+                nums1[x]=nums2[j];
+                j--;
             }
+            x--;
         }
-        while(i<m) {st.push(nums1[i]); i++;}
-        while(j<n) {st.push(nums2[j]); j++;}
-        for(int ii=nums1.length-1;ii>=0;ii--){
-            nums1[ii]=st.pop();
+        while(j>=0){
+            nums1[x]=nums2[j];
+            j--;
+            x--;
         }
-
-
         
     }
 }
