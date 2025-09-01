a120a082911eff100c1ba279f1bea8f590085a09
Haardik Mishra <117261665+HaardikMishra020@users.noreply.github.com>
2025-09-01 22:57:52 +0530

    Time: 1 ms (96.24%), Space: 45.6 MB (30.36%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int x=nums.length;
+        int []ans= new int[2*x];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[i];
+            ans[i+x]=nums[i];
+        }
+        return ans;
+        
+    }
+}
